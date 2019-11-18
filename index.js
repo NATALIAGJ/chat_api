b90088a155bf9a9a6690e56905ea41a7fbcd367d
Natalia Gonzalez Junco <gonzalezjunconatalia@gmail.com>
2019-11-18 16:35:23 -0500

    creacion del namespace y envio de mensajes a usuarios

--- index.js
+++ index.js
@@ -6,11 +6,27 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
-server.listen(5000, () => {
-  console.log(`Server running in http://localhost:${5000}`)
+io.on('connection', function(socket){
+    console.log(`new client: ${socket.id}`)
 })
 
-io.on('connection', function(socket){
-    //imprimiendo el id del cliente conectado 
-    console.log(`client: ${socket.id}`)
+/** namespace */
+const space = io.of('/namespace1');
+ 
+space.on('connection', function(socket){
+    console.log(`someone connected ${socket.id}`)
+    socket.on('message', (msg) => {
+        console.log(msg)
+        space.emit('message', msg)
+    })
+    socket.join('/room', function(){ 
+        console.log('uniendo al usuario en el room')
+        space.emit('hi', 'Hello everyone!')
+    })
+});
+ 
+space.emit('hola', { key:'data' })
+
+server.listen(5000, () => {
+    console.log(`Server running in http://localhost:${5000}`)
 })
